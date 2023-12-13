34fd34fb712a1c718f970a5c23fc88a7c9c558b9
JluSTEP <120753651+JluSTEP@users.noreply.github.com>
2023-12-14 00:47:33 +0300

    Update Func.h

--- Laba3.1.1/Func.h
+++ Laba3.1.1/Func.h
@@ -6,8 +6,8 @@ void addEl(List<T>& lst) {
 	T data;
 
 	system("cls");
-	cout << "<3<3<3<3<3 ADDING <3<3<3<3<3" << endl << endl;
-	cout << "-~<3 Enter yout data: ";
+	cout << " ADDING " << endl << endl;
+	cout << " Enter yout data: ";
 	cin >> data;
 
 	lst.push(data);
@@ -17,7 +17,7 @@ void addEl(List<T>& lst) {
 template<typename T>
 void removeEl(List<T>& lst) {
 	system("cls");
-	cout << "<3<3<3<3<3 REVOMING <3<3<3<3<3" << endl << endl;
+	cout << " REVOMING " << endl << endl;
 
 	lst.pop();
 	system("pause");
@@ -28,11 +28,11 @@ void showUnderIndex(List<T>& lst) {
 	int index;
 
 	system("cls");
-	cout << "<3<3<3<3<3 DISPLAY UNDER INDEX <3<3<3<3<3" << endl << endl;
-	cout << "-~<3 Enter index: ";
+	cout << " DISPLAY UNDER INDEX" << endl << endl;
+	cout << " Enter index: ";
 	cin >> index;
 
-	cout << endl << endl << "-~<3 Your element: " << lst[index];
+	cout << endl << endl << " Your element: " << lst[index];
 	cout << endl;
 	system("pause");
 }
@@ -40,8 +40,8 @@ void showUnderIndex(List<T>& lst) {
 template<typename T>
 void showList(List<T>& lst) {
 	system("cls");
-	cout << "<3<3<3<3<3 YOUR LIST <3<3<3<3<3" << endl << endl;
-	cout << "-~<3    ";
+	cout << "YOUR LIST" << endl << endl;
+	cout << "    ";
 	for (int i = 0; i < lst.size(); i++) {
 		cout << lst[i] << "  ";
 	}
@@ -52,9 +52,9 @@ void showList(List<T>& lst) {
 template<typename T>
 void checkEmpty(List<T>& lst) {
 	system("cls");
-	cout << "<3<3<3<3<3 EMPTY <3<3<3<3<3" << endl << endl;
+	cout << " EMPTY " << endl << endl;
 	cout << "| 0 = empty	|	1 = not empty |"  << endl << endl;
-	cout << "-~<3 Yout list: "<<lst.empty();
+	cout << "Yout list: "<<lst.empty();
 	cout << endl;
 	system("pause");
 }
@@ -63,15 +63,15 @@ template<typename T>
 void copyToNewList(List<T>& lst) {
 	List<T> lstSecond(lst);
 	system("cls");
-	cout << "<3<3<3<3<3 COPY TO NEW LIST <3<3<3<3<3" << endl << endl;
+	cout << " COPY TO NEW LIST " << endl << endl;
 
-	cout << "-~<3 Your first list: ";
+	cout << " Your first list: ";
 	for (int i = 0; i < lst.size();  i++) {
 		cout << lst[i] << "  ";
 	}
 	cout << endl << endl;
 
-	cout << "-~<3 Your new list: ";
+	cout << "Your new list: ";
 	for (int i = 0; i < lstSecond.size(); i++) {
 		cout << lstSecond[i] << "  ";
 	}
@@ -83,10 +83,10 @@ void copyToNewList(List<T>& lst) {
 template<typename T>
 void clearing(List<T>& lst) {
 	system("cls");
-	cout << "<3<3<3<3<3 CLEARING <3<3<3<3<3" << endl << endl;
+	cout << " CLEARING" << endl << endl;
 	lst.clear();
 	cout << "| 0 = empty	|	1 = not empty |"<< endl << endl;
-	cout << "-~<3 Yout list: " << lst.empty();
+	cout << " Yout list: " << lst.empty();
 	cout << endl;
 	system("pause");
 }
