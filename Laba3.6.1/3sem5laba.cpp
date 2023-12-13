f0728430057f2b57f753470cb789337108d88d10
JluSTEP <120753651+JluSTEP@users.noreply.github.com>
2023-12-14 00:56:35 +0300

    Add files via upload

---
+++ Laba3.6.1/3sem5laba.cpp
@@ -0,0 +1,53 @@
+﻿#define _CRTDBG_MAP_ALLOC
+#include <iostream>
+
+using namespace std;
+
+class MemoryLeakDetectorInitializer;
+
+class MemoryLeakDetector {
+public:
+	friend MemoryLeakDetectorInitializer;
+	static MemoryLeakDetector* getInstance() {
+		if (instance == nullptr) {
+			instance = new MemoryLeakDetector();
+		}
+		return instance;
+	}
+	void dumpMemoryLeaks() {
+		_CrtDumpMemoryLeaks();
+	}
+
+private:
+	static MemoryLeakDetector* instance;
+
+	MemoryLeakDetector() {};
+	~MemoryLeakDetector() {};
+
+};
+
+MemoryLeakDetector* MemoryLeakDetector::instance = nullptr;
+MemoryLeakDetector* leakDetector = nullptr;
+
+class MemoryLeakDetectorInitializer {
+public:
+	MemoryLeakDetectorInitializer() {
+		leakDetector = MemoryLeakDetector::getInstance();
+	}
+	~MemoryLeakDetectorInitializer() {
+		delete leakDetector;
+	}
+};
+
+MemoryLeakDetectorInitializer leakDetectorInitializer;
+
+int main() {
+	std::cout << "Hello World!\n";
+
+	int* utechka = new int[999999999];
+	//утечка памяти
+
+	leakDetector->dumpMemoryLeaks(); 
+
+	return 0;
+}
